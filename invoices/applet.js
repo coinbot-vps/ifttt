2cc5c3972c94b2cc4500427b5c31e640a8d1361c
Justin O'Neill <justin@oneezy.com>
2018-08-07 18:23:10 -0500

    added invoices filter code and verified that it's working

--- invoices/applet.js
+++ invoices/applet.js
@@ -1,14 +1,13 @@
-/* WHMCS to Gmail to Google Sheets to Discord
+/* Invoices
 ************************************************************************************
 
-IFTTT Applet:     https://platform.ifttt.com/p/coinbotvps/applets/nsqUGXwi
-Google Sheet:     https://drive.google.com/drive/u/1/folders/1CHzKF34mbZsNLc4QQBmyYk8WEXn30zC1
-Discord Channel:  https://discordapp.com/channels/441447726446542871/476130020113514508
+IFTTT Applet:     https://
+Google Sheet:     https://docs.google.com/spreadsheets/d/1Ecy8lHJ9bwACrKUttY9gGInnlCARQCVxZ-_khgJn2yE/edit#gid=0
+Discord Channel:  https://discordapp.com/channels/441447726446542871/476095000917049344
 Version:          1.0.0
-Description:      This applet controls 3 other IFTTT applets.
-                  - Tickets/Sales (https://ifttt.com/applets/83136757d-if-new-email-in-inbox-labeled-tickets-sales-then-add-row-to-coinbot-vps-s-google-drive-spreadsheet)
-                  - Tickets/Billing (https://ifttt.com/applets/83132049d-if-new-email-in-inbox-labeled-tickets-billing-then-add-row-to-coinbot-vps-s-google-drive-spreadsheet)
-                  - Tickets/Support (https://ifttt.com/applets/83131138d-if-new-email-in-inbox-labeled-tickets-support-then-add-row-to-coinbot-vps-s-google-drive-spreadsheet)
+Description:      This applet controls 2 other IFTTT applets.
+                  - Invoices/Paid
+                  - Invoices/Overdue
 
 *************************************************************************************/
 
@@ -17,9 +16,9 @@ Description:      This applet controls 3 other IFTTT applets.
 // Instead of writing `Twitter.newTweetByYou` or etc. every time you can shorten
 // it too `Trigger` because applet can has only one trigger at the moment.
 var Dept = GoogleSheets.newRowInSpreadsheet.ColumnA;
-var RecvTime = GoogleSheets.newRowInSpreadsheet.ColumnB;
-var FrmAdr = GoogleSheets.newRowInSpreadsheet.ColumnC;
-var FrmName = GoogleSheets.newRowInSpreadsheet.ColumnD;
+var ReceivedAt = GoogleSheets.newRowInSpreadsheet.ColumnB;
+var FromAddress = GoogleSheets.newRowInSpreadsheet.ColumnC;
+var FromName = GoogleSheets.newRowInSpreadsheet.ColumnD;
 var Subject = GoogleSheets.newRowInSpreadsheet.ColumnE;
 var BodyPlain = GoogleSheets.newRowInSpreadsheet.ColumnF;
 
@@ -27,6 +26,12 @@ var BodyPlain = GoogleSheets.newRowInSpreadsheet.ColumnF;
 // built-in JS functions or just assign it as hex number
 var Color = parseInt('1da1f2', 16);
 var Color2 = 0x1da1f2;
+
+var blue = 45311;
+var yellow = 16776960;
+var green = 65411;
+var red = 16056407;
+var gray = 9479342;
 
 // Meta.currentUserTime
 // Meta.triggerTime
@@ -42,13 +47,13 @@ var json = {
     "title": "Fresh from the Inbox of.. ",
     "description": Dept,
     "author": {
-      "name": FrmName,
-      "icon_url": "https://i.imgur.com/4XsVd2Y.jpg"
+      "name": FromName,
+      "icon_url": "https://coinbotvps.com/wp-content/themes/coinbotvps/images/logo/icon-square.png"
     },
       "fields": [
         {
           "name": "Email",
-          "value": FrmAdr
+          "value": FromAddress
         },
         {
           "name": "Department",
@@ -57,7 +62,7 @@ var json = {
         },
         {
           "name": "Received at ",
-          "value": RecvTime,
+          "value": ReceivedAt,
           "inline": true
         },
         {
@@ -73,7 +78,7 @@ var json = {
 // pretty neat, huh?
 
 // .set[field] methods allows you to replace action data
-MakerWebhooks.makeWebRequest.setUrl("https://discordapp.com/api/webhooks/476142922929274880/zot639fSIJuHXx9shgRG86lkXzBC6xR__owqOji8L6BvPZPLQEMar3uyuMOatMs1obEg");
+MakerWebhooks.makeWebRequest.setUrl("https://discordapp.com/api/webhooks/476505350787563521/mGS11_gz95I1V-BMPV009AHu6Y39knukRXwQWe_CKrUulEHNlgTZlCVD8L5Xp2PVHLpc");
 MakerWebhooks.makeWebRequest.setMethod("POST")
 MakerWebhooks.makeWebRequest.setContentType("application/json")
 MakerWebhooks.makeWebRequest.setBody(JSON.stringify(json));
